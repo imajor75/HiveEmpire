09831e61a136221d62aac22939ecba56cff54b4d
Major Imre <imewster@gmail.com>
2020-08-20 20:19:21 +0200

    Adding ground

---
+++ Assets/Ground.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundNode
+{
+    int x, y;
+    //Building building;
+    //Flag flag;
+    float height;
+}
+
+public class Ground : MonoBehaviour
+{
+    int width = 100, height = 100;
+    GroundNode[] layout;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log( "Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
