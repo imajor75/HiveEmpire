9014010baa4fd84fc6f0489ed660ca7e6a0f2dd5
Major Imre <imewster@gmail.com>
2020-09-13 13:16:25 +0200

    first

---
+++ Assets/Resource.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+	public Type type;
+	public int charges;
+
+	public enum Type
+	{
+		tree,
+		rock,
+		other
+	}
+
+	static public Resource Create()
+	{
+		GameObject obj = GameObject.CreatePrimitive( PrimitiveType.Capsule );
+		return obj.AddComponent<Resource>();
+	}
+
+	public Resource Setup( Type type, int charges = 1 )
+	{
+		this.type = type;
+		this.charges = charges;
+		return this;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
