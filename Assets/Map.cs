eb7f0cadaad52fcca70b474f8f7653d15a8a9fcd
imajor75 <imewster@gmail.com>
2021-06-21 17:24:32 +0200

    Fixed several minimap related issues

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
+[RequireComponent( typeof( CanvasGroup ) )]
 public class Map : Interface.Panel
 {
 	public MapImage content;
@@ -12,34 +13,39 @@ public class Map : Interface.Panel
 
 	static public Map Create()
 	{
-		return new GameObject().AddComponent<Map>();
+		return new GameObject( "Map" ).AddComponent<Map>();
 	}
 
 	public void Open( bool fullScreen = false )
 	{
-		Transform dialog = new GameObject( "Map dialog" ).transform;
-		dialog.transform.SetParent( transform );
 		this.fullScreen = fullScreen;
 		noCloseButton = fullScreen;
-		borderWidth = fullScreen ? 0 : 15;
+		borderWidth = fullScreen ? 0 : 20;
 		base.Open( null, 0, 0, 316, 316 );
-		name = "Map";
+
 		content = MapImage.Create();
 		content.Setup( fullScreen );
-		content.Stretch( 30, 30, -30, -30 ).Link( dialog );
+		content.Stretch( 20, 20, -20, -20 ).Link( this );
+
+		if ( fullScreen )
+		{
+			var c = gameObject.GetComponent<CanvasGroup>();
+			c.alpha = 0;
+			c.blocksRaycasts = false;
+		}
 	}
 
 	new void Update()
 	{
 		base.Update();
 
-		if ( Interface.GetKey( KeyCode.Equals ) )
+		if ( Interface.GetKey( KeyCode.KeypadPlus ) )
 			zoom *= 0.97f;
-		if ( Interface.GetKey( KeyCode.Minus ) )
+		if ( Interface.GetKey( KeyCode.KeypadMinus ) )
 			zoom *= 1.03f;
-		if ( Input.GetAxis( "Mouse ScrollWheel" ) > 0 )
+		if ( Input.GetAxis( "Mouse ScrollWheel" ) > 0 && fullScreen )
 			zoom *= 0.82f;
-		if ( Input.GetAxis( "Mouse ScrollWheel" ) < 0 )
+		if ( Input.GetAxis( "Mouse ScrollWheel" ) < 0 && fullScreen )
 			zoom *= 1.2f;
 		if ( zoom < zoomMin )
 			zoom = zoomMin;
@@ -49,6 +55,9 @@ public class Map : Interface.Panel
 		{
 			fullScreen = !fullScreen;
 			content.Setup( fullScreen );
+			var c = gameObject.GetComponent<CanvasGroup>();
+			c.alpha = fullScreen ? 0 : 1;
+			c.blocksRaycasts = !fullScreen;
 		}
 		if ( fullScreen )
 			World.instance.eye.moveSensitivity = zoom / 3;
@@ -57,36 +66,41 @@ public class Map : Interface.Panel
 		content.SetTarget( new Vector2( World.instance.eye.x, World.instance.eye.y ), zoom, rotation );
 	}
 
-	public class MapImage : MonoBehaviour
+	[RequireComponent( typeof( RawImage ) )]
+	public class MapImage : UIBehaviour
 	{
 		public RenderTexture renderTexture;
 		new public Camera camera;
 		public RawImage rawImage;
+		public bool fullScreen;
 
 		public static MapImage Create()
 		{
-			return new GameObject().AddComponent<MapImage>();
+			return new GameObject( "Map image" ).AddComponent<MapImage>();
 		}
 
 		public void Setup( bool fullScreen )
 		{
+			this.fullScreen = fullScreen;
+			var r = (transform as RectTransform).rect;
 			if ( !fullScreen )
-				renderTexture = new RenderTexture( 256, 256, 24 );
+				renderTexture = new RenderTexture( (int)r.width, (int)r.height, 24 );
 			else
 				renderTexture = null;
 
-			rawImage = gameObject.AddComponent<RawImage>();
-			name = "MapImage";
+			rawImage = gameObject.GetComponent<RawImage>();
 			rawImage.texture = renderTexture;
 
-			camera = new GameObject().AddComponent<Camera>();
-			camera.name = "MapCamera";
+			if ( !camera )
+				camera = new GameObject( "Map camera").AddComponent<Camera>();
 			camera.transform.SetParent( World.instance.ground.transform );
 			camera.targetTexture = renderTexture;
 			camera.cullingMask &= int.MaxValue - ( 1 << World.layerIndexNotOnMap );
 			camera.gameObject.AddComponent<CameraHighlight>();
 			if ( fullScreen )
 				Interface.root.viewport.SetCamera( camera );
+			else
+				Interface.root.viewport.SetCamera( null );
 		}
 
 		public void SetTarget( Vector2 position, float zoom, float rotation )
@@ -97,10 +111,22 @@ public class Map : Interface.Panel
 			camera.transform.rotation = Quaternion.Euler( 90, rotation, 0 );
 		}
 
-		void OnDestroy()
+		new void OnDestroy()
 		{
+			base.OnDestroy();
 			Destroy( camera.gameObject );
 			Interface.root.viewport.SetCamera( null );
+		}
+
+		new void OnRectTransformDimensionsChange()
+		{
+			if ( !fullScreen )
+			{
+				var r = (transform as RectTransform).rect;
+				if ( r.width > 0 && r.height > 0 )
+					rawImage.texture = camera.targetTexture = renderTexture = new RenderTexture( (int)r.width, (int)r.height, 24 );
+			}
+			base.OnRectTransformDimensionsChange();
 		}
 	}
 }
