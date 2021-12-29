c3b50bc90d2501db8c1a0bb2529d56005d67cb2b
imajor75 <imewster@gmail.com>
2021-12-30 00:07:09 +0100

    Fixed crash after loading a map

--- Assets/Workshop.cs
+++ Assets/Workshop.cs
@@ -719,6 +719,9 @@ public class Workshop : Building
 				SendItem( productionConfiguration.outputType, null, ItemDispatcher.Priority.high );
 		}
 
+		if ( mapIndicator == null )
+			return;
+			
 		mapIndicator.SetActive( true );
 		mapIndicator.transform.localScale = new Vector3( Constants.Node.size * productivity.current / 10, 1, Constants.Node.size * 0.02f );
 		mapIndicator.transform.rotation = Quaternion.Euler( 0, (float)( eye.direction / Math.PI * 180 ), 0 );
